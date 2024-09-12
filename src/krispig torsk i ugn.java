472ddc15ae29c38f3bb552ff25440c80a1b9cab4
Shams Alsaffar <shams.alsaffar@studerande.plushogskolan.se>
2024-09-12 13:17:24 +0200

    krispig

---
+++ src/krispig torsk i ugn.java
@@ -0,0 +1,31 @@
+public class kycklingyta {
+
+    /*Ingredienser:
+      //      6 torskfiléer (tinade eller färska portionsbitar) [ca 750 g]
+        //    1,5 tsk salt
+// 0,5 tsk malen svartpeppar
+// 0,5 msk citronskal, finrivet
+// 1 msk citronsaft, färskpressad
+// 1 dl olivolja
+// 1 msk färsk dragon, hackad
+// 1 dl persilja, hackad
+// 4 dl vitt bröd, finhackat (gärna surdeg)
+// 2 dl lagrad ost, riven (t ex västerbottensost)
+// 300 g körsbärstomater (på kvist)
+   // Till servering
+    // potatis, kokt och pressad
+       //     sallad
+    Instruktion:
+            1.Värm ugnen till 175 grader, varmluft.
+    Lägg fisken i en smord ugnsform och gnid in den runt om med salt,
+    peppar, citronskal och juice.
+2.
+        3.Blanda olja, dragon, persilja, bröd och ost i en skål.
+4.Fördela blandningen över fiskbitarna i formen.
+            5.Lägg körsbärstomaterna hela bredvid.
+    Tillaga mitt i ugnen i 15–25 minuter eller tills ytan är gyllene och
+    fisken är klar i mitten. Om du har en termometer ska
+    innertemperaturen vara cirka 52 grader.
+6.
+        7.Servera med pressad potatis och gärna en grönsallad.*/
+}
