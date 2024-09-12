e6c78a0794ce592eca97f6457a1f769bcf1f0a1e
Vinneys <Vincen.horvath@elev.ga.ntig.se>
2024-09-12 11:24:10 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("aeinfg");
+        System.out.println("eafia");
+        System.out.println("aieofna");
+        System.out.println("hej");
+        System.out.println("hehe");
     }
 }
