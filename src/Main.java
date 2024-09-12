e40de6826a83c2ad5b4bd81f8f28642d9b169401
simonkod2021 <simon98larsson@gmail.com>
2024-09-12 11:10:10 +0200

    Version 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("abc");
-        System.out.println("asdsa");
-        System.out.println("sdasda");
-        System.out.println("jasdajd");
+        System.out.println("sdkasjdkasjdk");
 
 
     }
